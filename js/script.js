a04e579010e1beb66fffaa78628cccc61ef1f52e
Bruno Assis <bhmassisbhmassis@gmail.com>
2022-02-22 15:45:41 -0300

    Update 2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
-
-if(window.innerWidth < 640){
-    $('#nav-items').hide();
-    
-    $('.menu').click(function(){
-        $('.nav-menu').toggle();
-
+$(function(){
+    $('#menu').click(function(){
+        $('#nav-menu').toggle(()=>{
+            $('#nav-menu').css('left', 0);
+        });
+        
+        
     })
-
-    
-}else{
-    $('.nav-menu').hide();
-    $('.menu').hide();
-}
+});
