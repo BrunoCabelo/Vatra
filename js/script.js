b9639e2a86594d25b8bf9002c8d326c3dfa1deca
Bruno Assis <bhmassisbhmassis@gmail.com>
2022-02-22 21:19:05 -0300

    JavaScript updates

--- js/script.js
+++ js/script.js
@@ -1,9 +1,43 @@
 $(function(){
+    var i = true;
     $('#menu').click(function(){
         $('#nav-menu').toggle(()=>{
             $('#nav-menu').css('left', 0);
         });
+
+        
+        if(i === true){
+            document.getElementById('menu').style.WebkitTransitionDuration='1s';
+            document.getElementById('menu').style.WebkitTransform = 'rotate(180deg)';
+            i = false;
+        }else{
+            document.getElementById('menu').style.WebkitTransitionDuration='1s';
+            document.getElementById('menu').style.WebkitTransform = 'rotate(0deg)';
+            i = true
+        }
+            
         
         
     })
+
+    $(".scroll").on("click", function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+    
+            const hash = this.hash;
+    
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset().top - 100
+                },
+                800
+            );
+        }
+
+        $('#nav-menu').toggle(()=>{
+            $('#nav-menu').css('left', 0);
+        });
+    });
+
+    
 });
